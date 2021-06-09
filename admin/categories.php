400aa0c1b01024c7063867fc76094c3397bd4ce1
JasonJerry <jasonjerry2105@hotmail.com>
2021-06-09 14:59:38 +0530

    09-06-2021 98

--- admin/categories.php
+++ admin/categories.php
@@ -18,7 +18,20 @@
                             <small>Author</small>
                         </h1>
                     
+                            <div class="col-xs-6">
+                            <form action="">
                             
+                            <div class="form-group">
+                            <label for="cat-title">Add Category </label>
+                            <input type="text" class="form-control" name="cat_title">
+
+                            </div>
+                            <div class="form-group">
+                            <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
+                            </div>
+                            </form>
+                            
+                            </div>
                     </div>
                 </div>
                 <!-- /.row -->
