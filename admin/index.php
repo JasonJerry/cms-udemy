100951a1a4dce3aa6192743209f4b7e2aea3c967
JasonJerry <jasonjerry2105@hotmail.com>
2021-06-09 14:35:30 +0530

    initial

---
+++ admin/index.php
@@ -0,0 +1,33 @@
+<?php include "includes/header.php" ?>
+
+<?php include "includes/navigation.php" ?>
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Welcome to Admin Panel
+                            <small>Author</small>
+                        </h1>
+                    
+                            
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+   
+<?php include "includes/footer.php" ?>
---
+++ includes/db.php
@@ -0,0 +1,28 @@
+<?php ob_start();
+$connection = mysqli_connect('localhost','root','','cms');
+
+
+
+// if($connection)
+// {
+//     echo "We are connected";
+// }
+
+
+//New method of connecting db
+// $db['DB_HOST'] = "localhost";
+// $db['DB_USER'] = "root";
+// $db['DB_PASS'] = "";
+// $db['DB_NAME'] = "cms";
+
+// foreach($db as $key => $value){
+// define($key, $value);
+// }
+
+// $connection = mysqli_connect($db['DB_HOST'], $db['DB_USER'] , $db['DB_PASS'] ,$db['DB_NAME']);
+// if($connection) {
+
+// echo "We are connected to the db successfully";
+
+// }
+?>
