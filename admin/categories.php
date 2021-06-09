804a4a8dca61a8225b484e34b246daa611bdacf9
JasonJerry <jasonjerry2105@hotmail.com>
2021-06-09 15:51:27 +0530

    Adding Categories @101

--- admin/categories.php
+++ admin/categories.php
@@ -19,7 +19,38 @@
                         </h1>
                     
                             <div class="col-xs-6">
-                            <form action="">
+                            <?php 
+                            if(isset($_POST['submit']))
+                            {
+                                // echo "<h1>hi from admin cat</h1>" ;
+                                $cat_title= $_POST['cat_title'];
+                                if($cat_title == "" || empty($cat_title))
+                                {
+                                    echo "The category name cannot be empty";
+                                }
+                                else
+                                {
+                                    $query = "INSERT INTO categories(cat_title) ";
+                                    $query .= "VALUE('{$cat_title}') ";
+
+                                    $create_category_query = mysqli_query($connection, $query);
+
+                                    if(!$create_category_query)
+                                    {
+                                        die('dead insert at category admin' .mysqli_error($connection));
+                                    }
+                                    else
+                                    {
+                                        echo "The category $cat_title was added successfully";
+                                    }
+                                }
+                            }
+                            
+                            ?>
+
+
+
+                            <form action="" method="post">
                             
                             <div class="form-group">
                             <label for="cat-title">Add Category </label>
@@ -34,6 +65,10 @@
                             </div>
 
                             <div class="col-xs-6">
+                            <?php
+                            $query = "SELECT * FROM categories";
+                            $select_categories = mysqli_query($connection, $query);
+                            ?>
                             <table class="table table-bordered table-hover">
                                 <thread>
                                     <tr>
@@ -42,10 +77,19 @@
                                     </tr>
                                 </thread>
                                 <tbody>
-                                <tr>
-                                <td>Baseball Category</td>
-                                <td>Cricket Category</td>
-                                </tr>
+
+                                <?php
+                            while ($row = mysqli_fetch_assoc($select_categories))
+                            {
+                                $cat_id = $row['cat_id'];
+                                $cat_title = $row['cat_title'];
+                                echo "<tr>";
+                                echo "<td>{$cat_id}</td>";
+                                echo "<td>{$cat_title}</td>";
+                                echo "</tr>";
+                            }
+                        ?>
+                                
                                 
                                 </tbody>
                             </table></div>
