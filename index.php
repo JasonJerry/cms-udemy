0f15c997d6653f6cea3510fdd17ccfe5b297d32e
JasonJerry <jasonjerry2105@hotmail.com>
2021-06-10 19:26:27 +0530

    Adding Posts and Links #120

--- index.php
+++ index.php
@@ -19,6 +19,8 @@
                 {
                     $post_title = $row['post_title'];
                     //echo "<li> <a href='#'> </a></li>";
+                    $post_id = $row['post_id'];
+                    $post_title = $row['post_title'];
                     $post_author = $row['post_author'];
                     $post_date = $row['post_date'];
                     $post_image = $row['post_image'];
@@ -32,14 +34,16 @@
 
                 <!-- First Blog Post -->
                 <h2>
-                    <a href="#"><?php echo $post_title; ?></a>
+                    <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
                 </h2>
                 <p class="lead">
                     by <a href="index.php"><?php echo $post_author; ?></a>
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date; ?></p>
                 <hr>
-                <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
+                <a href="post.php?p_id=<?php echo $post_id; ?>">
+                <img class="img-responsive" src="images/<?php echo $post_image; ?>"  alt="">
+                </a>
                 <hr>
                 <p><?php echo $post_content; ?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
