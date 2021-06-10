291c76518a98d2a88029f30ef2c35562780f2890
JasonJerry <jasonjerry2105@hotmail.com>
2021-06-10 21:27:45 +0530

    Added links in admin nav #121

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -63,7 +63,7 @@
             <div class="collapse navbar-collapse navbar-ex1-collapse">
                 <ul class="nav navbar-nav side-nav">
                     <li class="active">
-                        <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+                        <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
                     </li>
                     
                     <li>
