9732cd6ed048fd3aa5cdf116cf198d0f3b4ee6ce
JasonJerry <jasonjerry2105@hotmail.com>
2021-06-10 21:27:18 +0530

    Glitches in adding and editing posts resolved #120

--- admin/includes/add_posts.php
+++ admin/includes/add_posts.php
@@ -1,31 +1,49 @@
+<?php 
 
-<?php if(isset($_POST['create_post']))
+if(isset($_POST['create_post']))
 {
+   
     $post_title        = $_POST['title'];
-    $post_author       = $_POST['post_author'];
+    $post_author         = $_POST['post_author'];
     $post_category_id  = $_POST['post_category'];
     $post_status       = $_POST['post_status'];
+
     $post_image        = $_FILES['image']['name'];
     $post_image_temp   = $_FILES['image']['tmp_name'];
+
+
     $post_tags         = $_POST['post_tags'];
     $post_content      = $_POST['post_content'];
     $post_date         = date('d-m-y');
     $post_comment_count = 4;
-    //echo $post_image;
-    move_uploaded_file($post_image_temp, "../images/$post_image" );
 
 
-    $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date,post_image,post_content,post_tags,post_status) ";
-    $query .= "VALUES({$post_category_id},'{$post_title}','{$post_author}',now(),'{$post_image}','{$post_content}','{$post_tags}', '{$post_status}') ";        
-    $create_post_query = mysqli_query($connection, $query);  
-        
+    move_uploaded_file($post_image_temp, "../images/$post_image");
+
+    $query = "INSERT INTO posts(post_category_id, post_title,post_author,post_date,post_image,
+    post_content,post_tags,post_comment_count,post_status) ";
+
+    $query .= "VALUES('{$post_category_id}','{$post_title}','{$post_author}', now(),'{$post_image}','{$post_content}',
+    '{$post_tags}','{$post_comment_count}','{$post_status}' )";
+
+    $create_post_query = mysqli_query($connection, $query);
+
     confirmQuery($create_post_query);
 
-    $the_post_id = mysqli_insert_id($connection);
-    echo "<p class='bg-success'>Post Created. <a href='../post.php?p_id={$the_post_id}'>View Post </a> or <a href='posts.php'>Edit More Posts</a></p>";
-       
+
+
+
 }
+
+
+
+
+
+
 ?>
+
+
+
     <form action="" method="post" enctype="multipart/form-data">    
      
      
@@ -33,42 +51,45 @@
         <label for="title">Post Title</label>
          <input type="text" class="form-control" name="title">
      </div>
-<!-- 
-        <div class="form-group">
+
+        <!-- <div class="form-group">
       <label for="category">Category</label>
-      <select name="post_category_id" id="0">
+      <select name="post_category" id="">
       </select>
       
       </div> -->
-      <!-- <div class="form-group">
-        <label for="title">Post Category ID</label>
-         <input type="text" class="form-control" name="post_category_id">
-     </div> -->
-     <div class="form-group">
-        <!-- <label for="title">Post Category ID</label>
-         <input value="<?php //echo $post_category_id ; ?>" type="text" class="form-control" name="post_category_id"> -->
-        <select name="post_category" id="post_category">
-        
-        <?php  
 
-            $query = "SELECT * FROM categories ";
-                                        
-            $select_categories = mysqli_query($connection, $query);
 
-            confirmQuery($select_categories);
-            while ($row = mysqli_fetch_assoc($select_categories))
-            {
-                $cat_id = $row['cat_id'];
-                $cat_title = $row['cat_title'];
-                echo  "<option value='{cat_id}'>{$cat_title}</option>";
-            }
 
-        ?>
+
+
+      <div class="form-group">
+       <select name="post_category" id="post_category">
        
-        </select>
+       <?php 
+       
+       $query = "SELECT * FROM categories" ;
+       $select_categories = mysqli_query($connection, $query);
 
-     </div>
-                
+       confirmQuery($select_categories);
+
+           while ($row = mysqli_fetch_assoc($select_categories))
+           {
+               $cat_id = $row['cat_id']; 
+               $cat_title = $row['cat_title'];
+
+               echo "<option value = '{$cat_id}'>{$cat_title}</option>";
+
+       
+           }
+       ?>
+       </select>
+     </div>               
+
+
+
+
+
 
      <div class="form-group">
         <label for="title">Post Author</label>
--- admin/includes/edit_posts.php
+++ admin/includes/edit_posts.php
@@ -1,27 +1,29 @@
-<?php
+<?php 
 
-if(isset($_GET['p_id'])){
 
-    $the_post_id = $_GET['p_id'];
-}
-$query = "SELECT * FROM posts WHERE post_id = $the_post_id ";
-$select_posts_by_id = mysqli_query($connection, $query);
-
-while ($row = mysqli_fetch_assoc($select_posts_by_id))
+if(isset($_GET['p_id']))
 {
-    $post_id = $row['post_id'];
-    $post_author = $row['post_author'];
-    $post_title = $row['post_title'];
-    $post_category_id = $row['post_category_id'];
-    $post_status = $row['post_status'];
-    $post_image = $row['post_image'];
-    $post_tags = $row['post_tags'];
-    $post_comment_count = $row['post_comment_count'];
-    $post_date = $row['post_date'];
-    $post_content =$row['post_content'];//extra
+$the_post_id = $_GET['p_id'];
+
 }
 
-if(isset($_POST['update_post']))
+$query = "SELECT * FROM posts WHERE post_id = $the_post_id";
+$select_posts_by_id = mysqli_query($connection, $query);
+    while ($row = mysqli_fetch_assoc($select_posts_by_id))
+    {
+        $post_id = $row['post_id']; 
+        $post_author = $row['post_author'];
+        $post_title = $row['post_title'];
+        $post_category_id = $row['post_category_id'];
+        $post_status= $row['post_status'];
+        $post_image = $row['post_image'];
+        $post_content = $row['post_content'];
+        $post_tags = $row['post_tags'];
+        $post_comment_count = $row['post_comment_count'];
+        $post_date = $row['post_date'];
+
+    }
+if(isset(($_POST['update_post'])))
 {
     $post_author         =  $_POST['post_author'];
     $post_title          =  $_POST['post_title'];
@@ -31,8 +33,9 @@ if(isset($_POST['update_post']))
     $post_image_temp     =  $_FILES['image']['tmp_name'];
     $post_content        =  $_POST['post_content'];
     $post_tags           =  $_POST['post_tags'];
-    
+
     move_uploaded_file($post_image_temp, "../images/$post_image"); 
+
 
     if(empty($post_image)) {
         
@@ -44,82 +47,84 @@ if(isset($_POST['update_post']))
            $post_image = $row['post_image'];
         
         }
-        
-        
-}
-        $post_title = mysqli_real_escape_string($connection, $post_title);
+    }
 
-        
-          $query = "UPDATE posts SET ";
-          $query .="post_title  = '{$post_title}', ";
-          $query .="post_category_id = '{$post_category_id}', ";
-          $query .="post_date   =  now(), ";
-          $query .="post_author = '{$post_author}', ";
-          $query .="post_status = '{$post_status}', ";
-          $query .="post_tags   = '{$post_tags}', ";
-          $query .="post_content= '{$post_content}', ";
-          $query .="post_image  = '{$post_image}' ";
-          $query .= "WHERE post_id = {$the_post_id} ";
-        
-        $update_post = mysqli_query($connection,$query);
-        
-        confirmQuery($update_post);
-        
-        echo "<p class='bg-success'>Post Updated. <a href='../post.php?p_id={$the_post_id}'>View Post </a> or <a href='posts.php'>Edit More Posts</a></p>";
-        
+
+    $query = "UPDATE posts SET ";
+    $query .="post_title  = '{$post_title}', ";
+    $query .="post_category_id = '{$post_category_id}', ";
+    $query .="post_date   =  now(), ";
+    $query .="post_author = '{$post_author}', ";
+    $query .="post_status = '{$post_status}', ";
+    $query .="post_tags   = '{$post_tags}', ";
+    $query .="post_content= '{$post_content}', ";
+    $query .="post_image  = '{$post_image}' ";
+    $query .= "WHERE post_id = {$the_post_id} ";
+  
+    $update_post = mysqli_query($connection,$query);
+
+    confirmQuery($update_post);
 
 }
+
+
+
+
+
+
 ?>
-
-
-<form action="" method="post" enctype="multipart/form-data">    
+    <form action="" method="post" enctype="multipart/form-data">    
      
      
      <div class="form-group">
         <label for="title">Post Title</label>
-         <input value="<?php echo $post_title ; ?>" type="text" class="form-control" name="post_title">
+         <input value = "<?php echo $post_title; ?>" type="text" class="form-control" name="post_title">
      </div>
-<!-- 
-        <div class="form-group">
+
+        <!-- <div class="form-group">
       <label for="category">Category</label>
-      <select name="post_category_id" id="0">
+      <select name="post_category" id="">
       </select>
       
       </div> -->
+
       <div class="form-group">
-        <!-- <label for="title">Post Category ID</label>
-         <input value="<?php //echo $post_category_id ; ?>" type="text" class="form-control" name="post_category_id"> -->
-        <select name="post_category" id="post_category">
-        
-        <?php  
-
-            $query = "SELECT * FROM categories ";
-                                        
-            $select_categories = mysqli_query($connection, $query);
-
-            confirmQuery($select_categories);
-            while ($row = mysqli_fetch_assoc($select_categories))
-            {
-                $cat_id = $row['cat_id'];
-                $cat_title = $row['cat_title'];
-                echo  "<option value='{cat_id}'>{$cat_title}</option>";
-            }
-
-        ?>
+       <select name="post_category" id="post_category">
        
-        </select>
+       <?php 
+       
+       $query = "SELECT * FROM categories" ;
+       $select_categories = mysqli_query($connection, $query);
 
-     </div>
-              
+       confirmQuery($select_categories);
+
+           while ($row = mysqli_fetch_assoc($select_categories))
+           {
+               $cat_id = $row['cat_id']; 
+               $cat_title = $row['cat_title'];
+
+               echo "<option value = '{$cat_id}'>{$cat_title}</option>";
+
+       
+           }
+       ?>
+       
+       
+       
+       
+      
+     
+       </select>
+     </div>         
 
      <div class="form-group">
         <label for="title">Post Author</label>
-         <input value="<?php echo $post_author ; ?>" type="text" class="form-control" name="post_author">
+         <input value = "<?php echo $post_author; ?>"  type="text" class="form-control" name="post_author">
      </div>  
      
      <div class="form-group">
         <label for="title">Post Status</label>
-         <input value="<?php echo $post_status ; ?>" type="text" class="form-control" name="post_status">
+         <input value = "<?php echo $post_status; ?>" type="text" class="form-control" name="post_status">
      </div>  
 
 <!-- 
@@ -152,31 +157,33 @@ if(isset($_POST['update_post']))
         </select>
      </div> -->
      
-     
+    
+
      
    <div class="form-group">
-        <label for="post_image">Post Image</label>
-         <input type="file"  name="image">
-         <br>
-         <img width =200 src="../images/<?php echo $post_image; ?>" alt="Image">
-        
+   <label for="post_image">Post Image</label>
+   <input type="file"  name="image">
+   <br>
+       <img  width = 100 src="../images/<?php echo $post_image; ?> " alt="">
      </div>
 
      <div class="form-group">
         <label for="post_tags">Post Tags</label>
-         <input value="<?php echo $post_tags ; ?>" type="text" class="form-control" name="post_tags">
+         <input value = "<?php echo $post_tags; ?>" type="text" class="form-control" name="post_tags">
      </div>
      
      <div class="form-group">
         <label for="post_content">Post Content</label>
-        <textarea class="form-control "name="post_content" id="" cols="30" rows="10"><?php echo $post_content ; ?>
+        <textarea class="form-control "name="post_content" id="" cols="30" rows="10"><?php echo $post_content; ?>
+       
         </textarea>
      </div>
      
      
 
-     <div class="form-group">
-          <input class="btn btn-primary" type="submit" name="update_post" value="Update Post">
-      </div>
+      <div class="form-group">
+         <input class="btn btn-primary" type="submit" name="update_post" value="Edit Post">
+     </div>
+
 
 </form>
