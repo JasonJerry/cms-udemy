7a30cd34637e7f783c49f6f75f50d70f0d876ece
JasonJerry <jasonjerry2105@hotmail.com>
2021-06-11 17:12:08 +0530

    Adding comments section and making approved posts to be displayed

--- post.php
+++ post.php
@@ -57,62 +57,84 @@
 
 
                 <!-- Blog Comments -->
+                <?php
+                if(isset($_POST['create_comment']))
+                {
+                    $the_post_id = $_GET['p_id'];
+                    $comment_author = $_POST['comment_author'];
+                    $comment_email = $_POST['comment_email'];
+                    $comment_content = $_POST['comment_content'];
 
+                    $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status,comment_date)";
+                    $query .= "VALUES ($the_post_id ,'{$comment_author}', '{$comment_email}', '{$comment_content }', 'unapproved',now())";
+                    
+                    $create_comment_query = mysqli_query($connection, $query);
+                    if (!$create_comment_query) {
+                        die('QUERY FAILED in post.php create comment' . mysqli_error($connection));
+                    }
+                }
+
+
+
+                ?>
                 <!-- Comments Form -->
                 <div class="well">
                     <h4>Leave a Comment:</h4>
-                    <form role="form">
-                        <div class="form-group">
-                            <textarea class="form-control" rows="3"></textarea>
+                    <form action="" method="post" role="form">
+
+                    <div class="form-group">
+                    <label for="author" class="">Author</label>
+                            <input type="text" class="form-control" name="comment_author" id="comment_author">
                         </div>
-                        <button type="submit" class="btn btn-primary">Submit</button>
+                        <div class="form-group">
+                        <label for="email">Email</label>
+                            <input type="email" class="form-control" name="comment_email" id="comment_email">
+                        </div>
+                        <div class="form-group">
+                        <label for="comment">Comment</label>
+                            <textarea name="comment_content" class="form-control" rows="3"></textarea>
+                        </div>
+                        <button type="submit" name="create_comment" class="btn btn-primary">Submit</button>
                     </form>
                 </div>
 
                 <hr>
 
                 <!-- Posted Comments -->
+                <?php 
+                $query = "SELECT * FROM comments WHERE comment_post_id = {$the_post_id} ";
+                $query .= "AND comment_status = 'Approved' ";
+                $query .= "ORDER BY comment_id DESC ";
+                $select_comment_query = mysqli_query($connection, $query);
+                if(!$select_comment_query) 
+                {
 
+                    die('Query Failed' . mysqli_error($connection));
+                }
+                while ($row = mysqli_fetch_array($select_comment_query)) 
+                {
+                $comment_date   = $row['comment_date']; 
+                $comment_content= $row['comment_content'];
+                $comment_author = $row['comment_author'];
+                    
+                    ?>
                 <!-- Comment -->
                 <div class="media">
                     <a class="pull-left" href="#">
                         <img class="media-object" src="http://placehold.it/64x64" alt="">
                     </a>
                     <div class="media-body">
-                        <h4 class="media-heading">Start Bootstrap
-                            <small>August 25, 2014 at 9:30 PM</small>
+                        <h4 class="media-heading"><?php echo $comment_author; ?>
+                            <small><?php echo $comment_date; ?></small>
                         </h4>
-                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                        <?php echo $comment_content; ?>
                     </div>
                 </div>
 
-                <!-- Comment -->
-                <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/64x64" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading">Start Bootstrap
-                            <small>August 25, 2014 at 9:30 PM</small>
-                        </h4>
-                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                        <!-- Nested Comment -->
-                        <div class="media">
-                            <a class="pull-left" href="#">
-                                <img class="media-object" src="http://placehold.it/64x64" alt="">
-                            </a>
-                            <div class="media-body">
-                                <h4 class="media-heading">Nested Start Bootstrap
-                                    <small>August 25, 2014 at 9:30 PM</small>
-                                </h4>
-                                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                            </div>
-                        </div>
-                        <!-- End Nested Comment -->
-                    </div>
-                </div>
-
-
+               
+                <?php
+                }
+                ?>
 
 
             </div>
