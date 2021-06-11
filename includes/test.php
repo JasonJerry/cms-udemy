57580c2c2adffca36bf09d1a75c197240c98662a
JasonJerry <jasonjerry2105@hotmail.com>
2021-06-11 17:14:15 +0530

    Errors cleared and refactored #133

---
+++ includes/test.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<?php 
+
+$color = "red";
+
+switch($color)
+{
+    case "red";
+    echo "red";
+
+    case "blue";
+    echo "blue";
+
+    case "black";
+    echo "black";
+    break;
+}
+    
+    
+    
+?>
+
+
+
+</body>
+</html>
