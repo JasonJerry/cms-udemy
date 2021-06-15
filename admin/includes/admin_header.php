3edf0337aeb49867fcd141794236b08038cbcfd1
JasonJerry <jasonjerry2105@hotmail.com>
2021-06-15 18:06:49 +0530

    Added Summernote WYSIWYG #176

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -40,6 +40,14 @@ if(!isset($_SESSION['user_role']))
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+        <!-- include summernote css/js -->
+<!-- <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet"> -->
+
+<link rel="stylesheet" href="css/summernote.css">
+
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+
 </head>
 
 <body>
